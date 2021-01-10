b0941814240798a64dd3df8a1d361a3f11772345
GreenGhost27 <57357002+GreenGhost27@users.noreply.github.com>
2021-01-09 21:31:12 -0700

    Add files via upload

--- Mean_Median_Mode_Of_Data.py
+++ Mean_Median_Mode_Of_Data.py
@@ -36,36 +36,52 @@ def median():
         totalMedian = new_data[n//2]
     print("Median is:" + str(totalMedian))
 
-def mode():
-    with open('height-weight.csv', newline='') as f:
-        reader = csv.reader(f)
-        file_data = list(reader)
-    file_data.pop(0)
-    new_data=[]
-    for i in range(len(file_data)):
-        n_num = file_data[i][1]
-        new_data.append(n_num)
-    data = Counter(new_data)
-    mode_data_for_range = {
-        "50-60": 0,
-        "60-70": 0,
-        "70-80": 0
-    }
-    for height, occurence in data.items():
-        if 50 < float(height) < 60:
-            mode_data_for_range["50-60"] += occurence
-        elif 60 < float(height) < 70:
-            mode_data_for_range["60-70"] += occurence
-        elif 70 < float(height) < 80:
-            mode_data_for_range["70-80"] += occurence
-        mode_range, mode_occurence = 0,0
-        for range, occurence in mode_data_for_range.items():
-            if occurence > mode_occurence:
-                mode_range, mode_occurence = [int(range.split("-")[0]), int(range.split("-")[1])], occurence
-        mode = float((mode_range[0] + mode_range[1]) / 2)
-        print(f"Mode is: {mode:2f}")
 
-mode()
+
+def mode(sorted_data):
+    #Calculating Mode
+    data = Counter(sorted_data)
+    mode_data_for_range = {
+                            "50-60": 0,
+                            "60-70": 0,
+                            "70-80": 0,
+                            "80-90": 0
+                        }
+    for weight, occurence in data.items():
+        if 50 < weight < 60:
+            mode_data_for_range["50-60"] += occurence
+        elif 60 < weight < 70:
+            mode_data_for_range["60-70"] += occurence
+        elif 70 < weight < 80:
+            mode_data_for_range["70-80"] += occurence
+        elif 80 < weight < 90:
+            mode_data_for_range["80-90"] += occurence
+    mode_range, mode_occurence = 0, 0
+    for range, occurence in mode_data_for_range.items():
+        if occurence > mode_occurence:
+            mode_range, mode_occurence = [int(range.split("-")[0]), int(range.split("-")[1])], occurence
+    mode = float((mode_range[0] + mode_range[1]) / 2)
+    print(f"Mode is -> {mode:2f}")
+
+with open('height-weight.csv', newline='') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+#To remove headers from CSV
+file_data.pop(0)
+
+total_weight = 0
+total_entries = len(file_data)
+sorted_data = []
+
+for person_data in file_data:
+    total_weight += float(person_data[2])
+    sorted_data.append(float(person_data[2]))
+
+sorted_data.sort()
+
+
+mode(sorted_data)
 median()
 mean()
     
